e187454b810d291e83afdbb06f18ef0e0c655ac7
Bartłomiej Kuchnik <bkuchnik@onet.pl>
2026-01-07 13:21:11 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,8 @@
+
+const btn = document.getElementById('btn');
+const out = document.getElementById('output');
+
+btn.addEventListener('click', () => {
+  const now = new Date().toLocaleString('pl-PL');
+  out.textContent = `Klik! Aktualny czas: ${now}`;
+});
