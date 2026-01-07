a9c8787a5c027827e29d205373328fe417dbc2ed
Bartłomiej Kuchnik <bkuchnik@onet.pl>
2026-01-07 14:20:58 +0100

    fix 2

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 
-const btn = document.getElementById('btn');
-const out = document.getElementById('output');
+document.addEventListener('DOMContentLoaded', () => {
+  const btn = document.getElementById('btn');
+  const out = document.getElementById('output');
 
-btn.addEventListener('click', () => {
-  const now = new Date().toLocaleString('pl-PL');
-  out.textContent = `Klik! Aktualny czas: ${now}`;
-});
+  btn.addEventListener('click', () => {
+    const now = new Date().toLocaleString('pl-PL');
+    out.textContent = `Klik! Aktualny czas: ${now}`;
+   });
